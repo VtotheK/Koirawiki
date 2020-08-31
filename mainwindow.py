6331b9f6b1aee7996fd0e20f1d36457791876d8f
VilleK <ville.keituri1@gmail.com>
2020-08-31 13:35:26 +0300

    GUI work

--- mainwindow.py
+++ mainwindow.py
@@ -64,16 +64,38 @@ class Mainwindow(tk.Frame):
         self.rotuvalikko.bind("<<ComboboxSelected>>",self.rotuvalinta)
 
     def rotukuva(self):
-        self.canv = tk.Canvas(master=self.tietoframe,width=654, height=400)
-        self.canv.grid(row=0,column=0)
-
+        self.canv = tk.Canvas(master=self.tietoframe,width=656, height=400)
+        self.canv.grid(row=0,column=0,columnspan=25,rowspan=1)
     def rotukuvaus(self):
-        rotu_alkupera           = tk.Label(master=self.tietoframe,text="Rodun alkuperä:",bg=self.bg_col)
-        rotu_kayttotarkoitus    = tk.Label(master=self.tietoframe,text="Rodun alkuperäinen käyttötarkoitus:",bg=self.bg_col)
-        sakakorkeus             = tk.Label(master=self.tietoframe,text="Säkäkorkeus:",bg=self.bg_col)
-        paino                   = tk.Label(master=self.tietoframe,text="Paino:",bg=self.bg_col)
+        lblfont = ("Helvetica",16)
+        
+        f_alkupera = tk.Frame(self.tietoframe,bg=self.bg_col)
+        f_kayttotarkoitus = tk.Frame(self.tietoframe,bg=self.bg_col)
+        f_sakakorkeus= tk.Frame(self.tietoframe,bg=self.bg_col)
+        f_paino= tk.Frame(self.tietoframe,bg=self.bg_col)
+        
+        
+        f_alkupera.grid(column=0,row=1,sticky="nsew")
+        f_sakakorkeus.grid(column=0,row=3,sticky="nsew")
+        f_paino.grid(column=0,row=4,sticky="nsew")
+        f_kayttotarkoitus.grid(column=0,row=2,sticky="nsew")
 
+        rotu_alkupera           = tk.Label(f_alkupera,text="Rodun alkuperä:",bg=self.bg_col,font=lblfont)
+        rotu_kayttotarkoitus    = tk.Label(f_kayttotarkoitus,text="Rodun käyttötarkoitus:",bg=self.bg_col,font=lblfont)
+        sakakorkeus             = tk.Label(f_sakakorkeus,text="Säkäkorkeus:",bg=self.bg_col,font=lblfont)
+        paino                   = tk.Label(f_paino,text="Paino:",bg=self.bg_col,font=lblfont)
+        
+        json_alkupera           = tk.Label(f_alkupera,text="SUOMI",bg=self.bg_col,font=lblfont)
+        json_kayttotarkoitus    = tk.Label(f_kayttotarkoitus,text="Seurustelu/rottakoira",bg=self.bg_col,font=lblfont)
+        json_sakakorkeus        = tk.Label(f_sakakorkeus,text="150-2500cm",bg=self.bg_col,font=lblfont)
+        json_paino              = tk.Label(f_paino,text="5-25kg",bg=self.bg_col,font=lblfont)
+        
         rotu_alkupera.grid(row=1,column=0,sticky="w")
         rotu_kayttotarkoitus.grid(row=2,column=0,sticky="w")
         sakakorkeus.grid(row=3,column=0,sticky="w")
         paino.grid(row=4,column=0,sticky="w")
+        json_alkupera.grid(row=1,column=1,sticky="w")
+        json_kayttotarkoitus.grid(row=2,column=1)
+        json_sakakorkeus.grid(row=3,column=1,sticky="w")
+        json_paino.grid(row=4,column=1,sticky="w")
+         
